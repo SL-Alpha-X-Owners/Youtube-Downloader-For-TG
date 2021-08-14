a5f5244c2d41d615fde0869d0bc81a990342b489
SL Alpha-X Team <87191011+SL-Alpha-X-Team@users.noreply.github.com>
2021-08-14 08:57:16 +0530

    🚨 Added Bug Reporter Group & Sup ⚡

--- plugins/help.py
+++ plugins/help.py
@@ -6,6 +6,7 @@ async def start(client, message):
 	alpha2 = InlineKeyboardMarkup([
 		[InlineKeyboardButton("⚡ Whatsapp Sup 🗯", url="https://chat.whatsapp.com/Ku8MincABBWAEOaG44PqZE")],
 		[InlineKeyboardButton("🗂️ Report Bugs 🚨", url="https://t.me/AlphaX_SUPPORT")]
+                ])
 
 	help_image = "https://telegra.ph/file/8e3d7e8da2d02d3bd4b75.jpg"
 	await message.reply_photo(help_image,  caption="**💡 English HELP 📃...**\n \n__• Just Send your Youtube video url 🌟__ \n__• And i will give Method list to select your choice 😋__\n \n======================\n \n**💡 සින්හල උපදෙස් 📃...**\n \n__• කොපි කරගත් Youtube ලින්කුව එවන්න.__\n__• එවිට ලැබෙන ලැයිස්තුවෙන් අවශ්‍ය ප්‍රමාණය හා මාදිලිය තෝරාදෙන්න.__\n\n•••••••••••••••••••••••\n__• 😊 This bot is fully free.__\n`•⚙ Don't pay anyone for Bots like this.`\n\n⚡ ```Bot By Alpha-X Team``` 🚨\n",reply_markup=alpha2)
