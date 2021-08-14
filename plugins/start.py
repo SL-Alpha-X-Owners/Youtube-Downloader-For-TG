515284f2f3b08e65736fa4fb6f10e7022f4abb8d
SL Alpha-X Team <87191011+SL-Alpha-X-Team@users.noreply.github.com>
2021-08-14 09:00:47 +0530

    📌️ Updated Start Msg 🌺

--- plugins/start.py
+++ plugins/start.py
@@ -6,10 +6,10 @@ async def start(client, message):
     # return
     Alpha = InlineKeyboardMarkup([
         
-        [InlineKeyboardButton("⚡ Whatsapp •", url="https://chat.whatsapp.com/FOALYW6PMitLo9jntxEGqi")],
-        [InlineKeyboardButton("📣 Tg Group •", url="https://t.me/telegrm_music9")]
+        [InlineKeyboardButton("📌️ Whatsapp 🔎", url="https://chat.whatsapp.com/FOALYW6PMitLo9jntxEGqi")],
+        [InlineKeyboardButton("📌️ Tg Group 🔎", url="https://t.me/telegrm_music9")]
 
     ])
     thumbnail_url = "https://telegra.ph/file/35ba0a85f0e7f346e99ce.jpg"
-    await message.reply_photo(thumbnail_url, caption=f"**🙂 Hi <b>{message.from_user.first_name}**</b>\n\n<br>__😇 I Can Download YT Videos For You ✨️__</br>\n\n<b>• **🎑️ Instructions for use...🚨**</b>\n• **⚙ Type /help to get instructins...**\n \n───── ❝ **Lets Play** ❞ ─────\n ", reply_markup=Alpha)
+    await message.reply_photo(thumbnail_url, caption=f"**🙂 Hi <b>{message.from_user.first_name}**</b>\n\n<br>__😇 I Can Download YT Videos For You ✨️__</br>\n\n<b>• **🗂️ Instructions for use...🚨**</b>\n• **⚙ Type /help to get instructins...**\n \n───── ❝ **Lets Play** ❞ ─────\n ", reply_markup=Alpha)
     raise StopPropagation
