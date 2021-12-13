adedd2565fd0a71bc5f390987e3444d131309205
/ SL MaLiYa / <85741405+SL-Alpha-X@users.noreply.github.com>
2021-12-13 19:07:24 +0530

    V.2

--- config.py
+++
@@ -1,9 +0,0 @@
-import  os
-
-BOT_TOKEN = os.environ.get("BOT_TOKEN")
-APP_ID = int(os.environ.get("API_ID"))
-API_HASH = os.environ.get("API_HASH")
-youtube_next_fetch = 0 
-EDIT_TIME = 5
-
-# 🍁 Fast & High Quality YT Downloader Bot 🔥
